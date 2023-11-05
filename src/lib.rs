4480674d90b9d304a1459863e187326249292b63
niilz <niilz@outlook.de>
2023-11-05 22:30:02 +0100

    add propper scalar type matching

--- src/lib.rs
+++ src/lib.rs
@@ -50,30 +50,86 @@ pub fn generate_structs(path: TokenStream) -> TokenStream {
 }
 
 fn parse_proto(proto_data: &str) -> String {
-    let rustified = proto_data
-        .trim()
-        .replace("message", "struct")
-        .replace("optional ", "")
-        .replace("string", "String");
+    let trimmed = proto_data.trim();
+    let rustified = trimmed;
 
-    rustified
+    let mut lines = rustified
         .lines()
+        .map(|line| line.trim())
+        .filter(|line| !line.trim().is_empty())
+        .filter(|line| !line.trim().starts_with("//"))
+        .filter(|line| !line.trim().starts_with("/*"))
+        .filter(|line| !line.trim().starts_with("*"))
+        .filter(|line| !line.trim().starts_with("*/"));
+    if lines.next().unwrap() != "syntax = \"proto3\";" {
+        panic!("only proto3 is supported");
+    }
+    lines
         .map(|line| {
-            if line.ends_with(";") {
-                let (field, number) = line
-                    .split_once('=')
-                    .expect("proto-fields must have a number assigned");
-                let (typ, ident) = field
-                    .trim_end()
-                    .rsplit_once(" ")
-                    .expect("field must have a type and a ident");
-                format!("{}: {},", ident.trim(), typ.trim())
-            } else {
-                line.trim().to_string()
+            if line.contains("message") {
+                // Must be message start
+                if !line.ends_with("{") {
+                    panic!("messages must declare a block with open {{");
+                }
+                return line.replace("message", "struct");
             }
+            if line == "}" {
+                // end of message declaration
+                return line.to_string();
+            }
+
+            // we have a field (which must end with a semicolon)
+            if !line.ends_with(";") {
+                panic!("Field declarations must end with semicolon")
+            }
+
+            let (field, number) = line
+                .split_once('=')
+                .expect("proto-fields must have a number assigned");
+            let (typ, ident) = field
+                .trim_end()
+                .rsplit_once(" ")
+                .expect("field must have a type and a ident");
+            let rust_typ = to_rust_type(typ);
+            format!("{}: {},", ident.trim(), rust_typ.trim())
         })
         .collect::<Vec<_>>()
         .join("\n")
+}
+
+fn to_rust_type(proto_type: &str) -> String {
+    let is_optional = proto_type.contains("optional");
+    let typ = if is_optional {
+        proto_type.replace("optional", "")
+    } else {
+        proto_type.to_string()
+    };
+    let trimmed_typ = typ.trim();
+
+    let rust_typ = match trimmed_typ {
+        "double" => "f64",
+        "float" => "f32",
+        "int32" => "i32",
+        "int64" => "i64",
+        "uint32" => "u32",
+        "uint64" => "u64",
+        "sint32" => "i32",
+        "sint64" => "i64",
+        "fixed32" => "u32",
+        "fixed64" => "u64",
+        "sfixed32" => "u32",
+        "sfixed64" => "u64",
+        "bool" => "bool",
+        "string" => "String",
+        "bytes" => "[u8]",
+        _ => panic!("unsupported typ: '{typ}'"),
+    };
+
+    if is_optional {
+        format!("Option<{}>", rust_typ)
+    } else {
+        rust_typ.to_string()
+    }
 }
 
 #[cfg(test)]
@@ -81,10 +137,11 @@ mod test {
     use crate::parse_proto;
 
     const EXPECTED: &str = "struct Person {
-name: String,
+name: Option<String>,
 }";
 
-    const PROTO_PERSON: &str = "message Person {
+    const PROTO_PERSON: &str = "syntax = \"proto3\";
+message Person {
   optional string name = 1;
 }";
     #[test]
